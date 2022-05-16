42d297f290449999202e90e73f2c673f26e26647
Maulika <maulika26@gmail.com>
2022-05-15 22:37:17 -0400

    audio recording functionality

--- src/components/audioRecord.tsx
+++ src/components/audioRecord.tsx
@@ -1,31 +1,56 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import './assets/styles/main.scss'
 const MicRecorder = require('mic-recorder-to-mp3')
-const Mp3Recorder = new MicRecorder({ bitRate: 128})
+const Mp3Recorder = new MicRecorder({ bitRate: 128 })
 
 const AudioRecord = () => {
     const [blob, setBlob] = useState('');
     const [isReecording, setIsRecording] = useState(false);
+    const [isBlocked, setIsBlocked] = useState(false);
+
     const startRecording = () => {
-        Mp3Recorder
-        .start()
-        .then(() => {
-            setBlob('');
-            setIsRecording(true);
-        }).catch((e: any) => console.error(e))
+        if (isBlocked) {
+            alert('Permission Denied');
+        } else {
+            Mp3Recorder
+                .start()
+                .then(() => {
+                    setBlob('');
+                    setIsRecording(true);
+                }).catch((e: any) => console.error(e)
+            )
+        }
     }
+
     const stopRecording = () => {
         Mp3Recorder
-        .stop()
-        .getMp3()
-        .then(({ blob } : { blob : MediaSource}) => {
-            if (blob !== undefined) {
-                const Blob = URL.createObjectURL(blob)
-                setBlob(Blob);
-            }
-            setIsRecording(false);
-        }).catch((e: any) => console.log(e))
+            .stop()
+            .getMp3()
+            .then((blob : any) => {
+                if (blob !== undefined) {
+                    const blobURL = URL.createObjectURL(new Blob(blob, {type: "audio/mp3"}))
+                    setBlob(blobURL);
+                }
+                setIsRecording(false);
+            }).catch((e: any) => console.log(e)
+        );
     }
+
+    useEffect(() => {
+        let newVariable: any;
+        newVariable = navigator;
+
+        newVariable.getUserMedia({ audio: true },
+            () => {
+                console.log('Permission Granted');
+                setIsBlocked(false);
+            },
+            () => {
+                console.log('Permission Denied');
+                setIsBlocked(true)
+            },
+        );
+    })
 
     return <div className="container">
         <div className="main-component">
